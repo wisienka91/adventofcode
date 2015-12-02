8b64d5850f346c341811aaf1eb52350d8d65245a
wisienka91 <wisnia91@gmail.com>
2015-12-03 00:31:50 +0100

    Day01, Gitignore

---
+++ day01/day01.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+def count_the_floor(input_data):
+    counter = 0
+    basement = -1
+    basement_found = False
+    for i, ch in enumerate(input_data):
+        if ch == '(':
+            counter += 1
+        elif ch == ')':
+            counter -= 1
+        if counter == -1 and not basement_found:
+            basement = i+1
+            basement_found = True
+    return (counter, basement)
+
+def get_the_floor():
+    input_file = open('day01.txt', 'r')
+    input_data = input_file.readline()
+    floor, basement = count_the_floor(input_data)
+    print 'Floor: %s\nBasement: %s' % (floor, basement)
+
+
+if __name__ == "__main__":
+    get_the_floor()
+
