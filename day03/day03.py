7de2a6a3a8d65bc38be69ff8b7b50e21da6e5376
wisienka91 <wisnia91@gmail.com>
2015-12-05 09:13:58 +0100

    Day03 - full

--- day03/day03.py
+++ day03/day03.py
@@ -1,48 +1,66 @@
 # -*- coding: utf-8 -*-
 
 class GiftDeliverySystem(object):
-    visited_santa_house = (0, 0)
-    visited_robo_house = (0, 0)
-    distinct_santa_houses = [(0, 0)]
 
-    def get_new_house(self, direction):
-        new_houses = [None, None]
+    def __init__(self):
+        self.visited_santa_house = (0, 0)
+        self.visited_robo_house = (0, 0)
+        self.distinct_houses = [(0, 0)]
+
+    def get_new_house(self, direction, visited):
+        new_house = None
         if direction == '^':
-            new_houses[0] = (
-                self.visited_santa_house[0], self.visited_santa_house[1] + 1)
+            new_house = (visited[0], visited[1] + 1)
         elif direction == 'v':
-            new_houses[0] = (
-                self.visited_santa_house[0], self.visited_santa_house[1] - 1)
+            new_house = (visited[0], visited[1] - 1)
         elif direction == '>':
-            new_houses[0] = (
-                self.visited_santa_house[0] + 1, self.visited_santa_house[1])
+            new_house = (visited[0] + 1, visited[1])
         elif direction == '<':
-            new_houses[0] = (
-                self.visited_santa_house[0] - 1, self.visited_santa_house[1])
-        return new_houses
+            new_house = (visited[0] - 1, visited[1])
+        return new_house
 
-    def check_new_santa_house(self, direction):
-        next_santa_house = self.get_new_house(direction)[0]
-        if (next_santa_house and 
-            (next_santa_house not in self.distinct_santa_houses)
-        ):
-            self.distinct_santa_houses.append(next_santa_house)
+    def set_visited(self, next_house, santa):
+        if next_house not in self.distinct_houses:
+            self.distinct_houses.append(next_house)
+        if santa == 'santa':
+            self.visited_santa_house = next_house
+        elif santa == 'robo':
+            self.visited_robo_house = next_house
 
-        self.visited_santa_house = next_santa_house
+    def visit_new_house(self, direction, santa):
+        visited = self.visited_santa_house
+        if santa == 'robo':
+            visited = self.visited_robo_house
+        next_house = self.get_new_house(direction, visited)
+        if next_house:
+            self.set_visited(next_house, santa)
 
+    def pairwise(self, santas_path):
+        path = iter(santas_path)
+        while True:
+            yield next(path), next(path)
 
-    def get_distinct_houses(self, santas_path):
+    def follow_one_path(self, santas_path):
         for direction in santas_path:
-            self.check_new_santa_house(direction)
-        return len(self.distinct_santa_houses)
+            self.visit_new_house(direction, 'santa')
+        return len(self.distinct_houses)
+
+    def follow_two_paths(self, santas_path):
+        path = self.pairwise(santas_path)
+        for santa, santa2 in path:
+            self.visit_new_house(santa, 'santa')
+            self.visit_new_house(santa2, 'robo')
+        return len(self.distinct_houses)
 
     def count_santa_houses_alone(self):
         input_file = open('day03.txt', 'r')
-        visited_houses_count = self.get_distinct_houses(input_file.readline())
+        visited_houses_count = self.follow_one_path(input_file.readline())
         print 'Visited houses: %s' % visited_houses_count
 
     def count_santa_and_robo_houses(self):
-        return None
+        input_file = open('day03.txt', 'r')
+        visited_houses_count = self.follow_two_paths(input_file.readline())
+        print 'Visited houses: %s' % visited_houses_count
 
 if __name__ == "__main__":
     GiftDeliverySystem().count_santa_houses_alone()
