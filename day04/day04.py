dc52ed5c1fd5a9fb972218fa16d8e1b1c8964a30
wisienka91 <wisnia91@gmail.com>
2016-05-07 17:48:54 +0200

    Day04 task content & refactoring

--- day04/day04.py
+++ day04/day04.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import hashlib
 
-SECRET_KEY = 'bgvyzdsv'
-
 def get_md5_hash(s_key):
     return hashlib.md5(s_key).hexdigest()
 
@@ -12,18 +10,24 @@ def is_nonce_good(md5_str, condition):
         return True
     return False
 
-def get_ideal_nonce(condition):
+def get_secret_key():
+    input_file = open('day04.txt', 'r')
+    secret_key = input_file.readline().rstrip()
+    return secret_key
+
+def get_ideal_nonce(condition, secret_key):
     nonce = 0
-    key_with_nonce = '%s%s' % (SECRET_KEY, nonce)
+    key_with_nonce = '%s%s' % (secret_key, nonce)
     while not is_nonce_good(key_with_nonce, condition):
         nonce += 1
-        key_with_nonce = '%s%s' % (SECRET_KEY, nonce)
+        key_with_nonce = '%s%s' % (secret_key, nonce)
     print 'Ideal nonce for condition %s is: %s' % (condition, nonce)
 
 def get_ideal_nonces():
+    secret_key = get_secret_key()
     conditions = ['00000', '000000']
     for condition in conditions:
-        get_ideal_nonce(condition)
+        get_ideal_nonce(condition, secret_key)
 
 if __name__ == '__main__':
     get_ideal_nonces()
