5a2754c9afb1b731b817bd931a60f30ab6a84cde
Maciej W <wisnia91@gmail.com>
2020-12-01 22:51:39 +0100

    2020 day01

---
+++ 2020/day01/day01.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from itertools import combinations
+
+
+def _get_the_numbers():
+    numbers = []
+    taskfile = open('day01.txt', 'r')
+
+    for line in taskfile.readlines():
+        numbers.append(int(line))
+    taskfile.close()
+
+    return numbers
+
+
+def find_the_sum_product(sum_for_product, amount):
+    numbers = _get_the_numbers()
+    answer = None
+    product = 1
+    addends = combinations(numbers, amount)
+
+    for i in addends:
+        if sum(i) == sum_for_product:
+            answer = i
+    for j in answer:
+        product *= j
+
+    return product
+
+
+if __name__ == '__main__':
+    sum_for_product = 2020
+    sum_2_product = find_the_sum_product(sum_for_product, 2)
+    sum_3_product = find_the_sum_product(sum_for_product, 3)
+    print('Product of 2020 two numbers sum is: {}'.format(sum_2_product))
+    print('Product of 2020 three numbers sum is: {}'.format(sum_3_product))
+
